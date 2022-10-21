f88064dfb713c784df9f39c9cda04ac8437572d8
lorenanfante <linfante@modyo.com>
2022-10-21 09:16:57 -0300

    feat:generators with a short story

---
+++ generators.js
@@ -0,0 +1,20 @@
+function* shopping(){
+  //stuff on the sidewalk
+
+  //walking down the sidewalk
+
+  //go into the store with cash
+  const stuffFromStore = yield 'cash';
+  
+  //walking back home
+  return stuffFromStore;
+}
+
+//stuff in the store
+const gen = shopping(); //"just calling "shopping" doesn't actually invoke any code whatsoever"
+gen.next(); // leaving the house, here it enters to the function until yield
+// walk into the store
+// walking up and down the aisles
+// purchase our stuff
+// 
+gen.next('groceries'); // leaving the store with groceries
