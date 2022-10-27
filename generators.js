a6f86d006d8bdaf7e413522d7dd2b76674a11a5a
lorenanfante <linfante@modyo.com>
2022-10-27 15:52:14 -0300

    feat: generators and recursion

--- generators.js
+++ generators.js
@@ -71,3 +71,31 @@ for(let name of engineeringTeam){
   names.push(name);
 }
 names;
+
+//Generators with recursion
+class Comment {
+  constructor(content, children){
+    this.content = content;
+    this.children= children;
+  }
+  *[Symbol.iterator]() {
+    yield this.content;
+    //Array helpers like map or forEach DO NOT work with generators!
+    for(let child of this.children){
+      yield* child;
+    }
+  }
+}
+
+const children = [
+  new Comment ('good comment', []),
+  new Comment ('bad comment', []),
+  new Comment ('meh', [])  
+];
+
+const tree = new Comment('Great post!', children);
+const values = [];
+for(let value of tree){
+  values.push(value);
+}
+values;
