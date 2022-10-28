8f67095f00b568df846692b8d322ac749ab39707
lorenanfante <linfante@modyo.com>
2022-10-28 10:48:47 -0300

    feat: async code with promises

--- promises.js
+++ promises.js
@@ -1,8 +1,19 @@
 promise = new Promise((resolve, reject) => {
-  resolve();
+  setTimeout(() => {
+    resolve();
+  }, 3000);
 });
 //when is rejected an this error comes up "Uncaught (in promise) undefined". It means that we need to add the catch
 promise
-  .then(() => console.log('Fnally Finished!')) //.then ONLY will be called if the promise is resolved!
+  .then(() => console.log('Finally Finished!')) //.then ONLY will be called if the promise is resolved!
   .then(() => console.log('I also ran!!'))
   .catch( () => console.log('oh oh'));
+
+  /* setTimeout is working under the hood like this 
+  var request = new XHTMLRequest ()
+    //make request
+    request.onload( () => {
+
+    };);
+
+  */
