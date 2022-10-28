2a0f79a408d3995f1e44fa42625936254ac164ab
lorenanfante <linfante@modyo.com>
2022-10-28 10:38:33 -0300

    feat: then and catch

--- promises.js
+++ promises.js
@@ -2,3 +2,7 @@ promise = new Promise((resolve, reject) => {
   resolve();
 });
 //when is rejected an this error comes up "Uncaught (in promise) undefined". It means that we need to add the catch
+promise
+  .then(() => console.log('Fnally Finished!')) //.then ONLY will be called if the promise is resolved!
+  .then(() => console.log('I also ran!!'))
+  .catch( () => console.log('oh oh'));
